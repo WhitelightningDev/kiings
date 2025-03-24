54420f6b0d21967f5c6807234a56be4e619f64a6
Daniel <danielmommsen2@gmail.com>
2025-03-24 15:51:52 +0200

    updated the test wash

--- src/functions/washData.js
+++ src/functions/washData.js
@@ -1,5 +1,5 @@
 export const washTypes = [
-  {name: "TEXTWASH", price: .10, details: "(text wash)"},
+  {name: "Test Wash payment", price: 1, details: "(test wash payment)"},
   { name: "Wash n Go", price: 80, details: "(Shampoo & Wax, Tire Shine)" },
   {
     name: "Wash & Vacuum",
