2702b1f19052f2da47030cd3f9f7d5a0cd86d796
Daniel <danielmommsen2@gmail.com>
2025-03-24 11:43:41 +0200

    Updated the canceled payment page

--- src/pages/PaymentCanceled.js
+++ src/pages/PaymentCanceled.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom'; // Correct hook for React Router v6
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function PaymentCanceled() {
   const navigate = useNavigate(); // Use navigate instead of useHistory
@@ -10,17 +10,15 @@ function PaymentCanceled() {
   };
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Payment Canceled
-      </Typography>
-      <Typography variant="body1" gutterBottom>
+    <div className="container rounded-4 shadow text-center bg-danger p-3 mt-5">
+      <h2 className="mb-3 text-light">Payment Canceled</h2>
+      <p className="mb-4 text-light">
         You have canceled your payment. If this was a mistake, you can try again later.
-      </Typography>
-      <Button variant="contained" color="primary" onClick={handleHomeRedirect}>
+      </p>
+      <button className="btn text-light" onClick={handleHomeRedirect}>
         Go to Homepage
-      </Button>
-    </Container>
+      </button>
+    </div>
   );
 }
 
