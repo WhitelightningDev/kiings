60877b70085420a800fe6b54bd236a02f27f7f32
Daniel <danielmommsen2@gmail.com>
2025-03-24 12:03:09 +0200

    Updated the navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,32 @@ import { NavLink } from 'react-router-dom';
 import NavLogo from '../assets/kings-logo.png';
 import '../styles/css/navbar.css';
 import '../App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg mb-2">
-      <div className="container-fluid">  {/* Use container-fluid for full-width navbar */}
+      <div className="container"> {/* Centered container for better layout */}
+        {/* Logo & Branding */}
         <NavLink to="/" className="navbar-brand d-flex align-items-center">
-          <img className='nav-logo me-2' src={NavLogo} alt='nav-logo' width="50" height="50" />
+          <img className="nav-logo me-2" src={NavLogo} alt="nav-logo" width="50" height="50" />
           <span className="fs-4 fw-bold">Kiings Mobile Car Wash</span>
         </NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+        {/* Mobile Toggle Button */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
+
+        {/* Navbar Links */}
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
